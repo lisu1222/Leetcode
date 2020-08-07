2fee95b962907b0f86c066bb178067b501d2a7b2
LI SU <sulisunny@hotmail.com>
2020-08-07 11:34:33 -0600

    leetcode 20 easy

---
+++ validParentheses.py
@@ -0,0 +1,58 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+"""
+
+def isValid(s):  
+	#stack, append and pop, complexity O(n)
+	bracket_map = {"(":")", "[":"]","{":"}"}
+	open_par = set(["(","[","{"])
+	stack = []
+
+	for i in s:
+		if i in open_par:
+			stack.append(i)
+		elif stack and i == bracket_map[stack[-1]]:
+			stack.pop()
+		else:
+			return False 
+	return stack == []
+
+
+
+
+def isValid(s):
+	#string replacement, complexity O(n)
+	while "()" or "[]" or "{}" in s:
+		s = s.replace("()", "").replace("[","").replace("{","")
+	return s == ''
+
+
+
+
+
+
+
+
+
+
+
+
